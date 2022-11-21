611b93f517fe2e4b61d0d8c4d842ded1ff449a5a
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-11-21 11:41:58 +0700

    add more image on products

--- src/components/common/list-products-landing-page/ListImageProducts.tsx
+++ src/components/common/list-products-landing-page/ListImageProducts.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import listProductData from "../../../data/product-data/productData.json";
 import Logo from "../../../assets/icons/gundam-icon.png";
 
+export interface DataProductsProps {
+  id: number;
+  src: string;
+  alt: string;
+}
+
 const ListImageProducts = () => {
+  const [dataProducts, setDataProducts] = useState<DataProductsProps[]>([]);
+
+  useEffect(() => {
+    if (listProductData) {
+      setDataProducts(listProductData.slice(0, 4));
+    }
+  }, []);
+
+  const loadMore = () => {
+    const moreData = listProductData.slice(4);
+    setDataProducts((prevData) => [...prevData, ...moreData]);
+  };
+
   return (
     <section className="machines-wrapper py-6 rounded-sm bg-gradient-to-br from-gray-100 via-gray-300 to-white ">
       <div className="eva-banner-container overflow-hidden max-h-[500px] mb-6  px-5 ">
@@ -13,31 +34,28 @@ const ListImageProducts = () => {
         />
       </div>
       <div className="top-row-card-machine gap-6 grid lg:grid-cols-4 max-sm:grid-cols-1 md:grid-cols-2 px-5">
-        <LazyLoadImage
-          src="https://www.projectgunpla.com/gundam/zaku-ortega.png"
-          alt="gundam"
-          placeholderSrc={Logo}
-          className="object-cover"
-        />
-        <LazyLoadImage
-          src="https://www.projectgunpla.com/gundam/zaku-cannon.png"
-          alt="gundam"
-          placeholderSrc={Logo}
-          className="object-cover"
-        />
-        <LazyLoadImage
-          src="https://www.projectgunpla.com/gundam/zaku-char.png"
-          alt="gundam"
-          placeholderSrc={Logo}
-          className="object-cover"
-        />
-        <LazyLoadImage
-          src="https://www.projectgunpla.com/gundam/zaku.png"
-          alt="gundam"
-          placeholderSrc={Logo}
-          className="object-cover"
-        />
+        {dataProducts &&
+          dataProducts.map((product) => (
+            <LazyLoadImage
+              key={product.id}
+              src={product.src}
+              alt={product.alt}
+              placeholderSrc={Logo}
+              title={product.alt}
+              className="object-cover"
+            />
+          ))}
       </div>
+      {listProductData.length !== dataProducts.length ? (
+        <button
+          className="mt-8 py-3 px-6 delay-75 text-white bg-red-300 hover:bg-violet-300 active:bg-red-300 rounded-full"
+          onClick={loadMore}
+        >
+          More Product
+        </button>
+      ) : (
+        <></>
+      )}
     </section>
   );
 };
