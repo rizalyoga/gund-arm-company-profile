cc972d520579325bb440f684cb7fee8cf29fa123
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-01-26 21:53:35 +0700

    show error message

--- src/pages/news-page/NewsPage.tsx
+++ src/pages/news-page/NewsPage.tsx
@@ -17,8 +17,27 @@ const News = () => {
     getNewsLaunchesPast();
   }, []);
 
-  if (error) console.error(error);
   if (loading) return <Loading />;
+  if (error) {
+    console.error(error);
+    return (
+      <div className="line-grid-container">
+        <div className="containers mt-12 min-h-screen ">
+          <h1 className="font-lobster text-7xl font-extrabold py-4 text-transparent bg-clip-text bg-gradient-to-r from-red-300 via-purple-400 to-blue-300">
+            Gund•Arm News
+          </h1>
+          <h3 className="-mt-5 text-xl font-poppins text-violet-300 italic font-semibold">
+            •This content from SpaceX graphQL•
+          </h3>
+          <section className="card-wraper  ">
+            <h1 className="text-center mt-32 text-2xl font-semibold text-red-400">
+              error message : {error.message}
+            </h1>
+          </section>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="line-grid-container ">
